e8b5a1a4e2a7556e16a5b72e16f035ea673099f4
pondels <yeckron@comcast.net>
2021-09-29 23:20:23 -0600

    Added Website integration
    Ignore yourmom.py I'm using that for testing librosa and aubio :P

--- yourmom.py
+++ yourmom.py
@@ -1,3 +1,4 @@
 import aubio
 
-print(aubio.slice_source_at_stamps("coconut.wav", [1]))
+aubio.slice_source_at_stamps('coconut.wav', [44100], [44100 * 2 - 1])
+# print(aubio.slice_source_at_stamps("coconut.wav", timestamps=[15]))

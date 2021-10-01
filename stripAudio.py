2a9cad85869dfc93f86075220d266075ad110004
Joe Cheney <Pretzelman718@gmail.com>
2021-10-01 17:14:39 -0600

    Let's try that again

---
+++ stripAudio.py
@@ -0,0 +1,9 @@
+# https://towardsdatascience.com/extracting-audio-from-video-using-python-58856a940fd
+
+import moviepy.editor as mp
+
+my_clip = mp.VideoFileClip(f"ZPqZyIKtW0Y.mp4")
+
+my_clip
+
+#This is still unfinished. Still need to add the function to get the audio out

a1b3ce74b0e0dc4073f0785fa670cafec4f209cc
ValiantWolf <wit19011@byui.edu>
2021-10-05 15:36:24 -0600

    colour class

---
+++ colour.py
@@ -0,0 +1,29 @@
+import librosa as librosa
+
+class Colour():
+
+    def __init__(self):
+        self.colours = {
+            "A":(117,0,0),
+            "A♯":(255,236,0),
+            "B":(153,255,0),
+            "C":(40,255,0),
+            "C♯":(0,255,232),
+            "D":(0,124,255),
+            "D♯":(5,0,255),
+            "E":(69,0,234),
+            "F":(87,0,158),
+            "F♯":(116,0,0),
+            "G":(179,0,0),
+            "G♯":(238,0,0)
+        }
+
+    def note2colour(self, freq):
+        noteName = librosa.hz_to_note(freq)
+        
+        noteColour = self.colours.get(noteName[:-1])
+
+        return noteColour
+
+colour = Colour()
+colour.note2colour(695)

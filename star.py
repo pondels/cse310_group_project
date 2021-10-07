88b01e21c1dc129568c727440bc91a30425afb1c
pondels <72519395+pondels@users.noreply.github.com>
2021-10-07 16:19:51 -0600

    star class skeleton made

---
+++ star.py
@@ -0,0 +1,18 @@
+
+
+class Star():
+
+    def __init__(self):
+        pass
+
+    def length(self):
+        '''
+            Returns the length of the "star"
+        '''
+        pass
+
+    def time(self):
+        '''
+            Returns how long the "star" stays on screen for
+        '''
+        pass

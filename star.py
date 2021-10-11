f5954c816035fa228808441fd8c8b42990c9c797
Joe Cheney <Pretzelman718@gmail.com>
2021-10-11 13:28:07 -0600

    TODOs, add comments plz

--- star.py
+++ star.py
@@ -1,18 +1,11 @@
 import random
 
-
-# This will generate a random point on the edge of the screen.
-# Designed for 1280×720 resolution
-def randEdge(self):
-    side = random.randrange(3)
-
-    return {
-        0: (0,random.randrange(720)),
-        1: (1280,random.randrange(720)),
-        2: (random.randrange(1280),0),
-        3: (random.randrange(1280),720)
-    }[side]
-
+"""
+TODO
+Class: Star
+Description: What does it do? What does it output? What does it use?
+Are we even using this any more?
+"""
 class Star():
 
     def __init__(self):

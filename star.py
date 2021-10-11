60c5ccb8309b4e784155006fd48c7e693134bc3e
jmc718 <Pretzelman718@gmail.com>
2021-10-11 11:22:01 -0600

    Star knows where it is STARting now

--- star.py
+++ star.py
@@ -1,8 +1,24 @@
+import random
 
+
+# This will generate a random point on the edge of the screen.
+# Designed for 1280×720 resolution
+def randEdge(self):
+    side = random.randrange(3)
+
+    return {
+        0: (0,random.randrange(720)),
+        1: (1280,random.randrange(720)),
+        2: (random.randrange(1280),0),
+        3: (random.randrange(1280),720)
+    }[side]
 
 class Star():
 
     def __init__(self):
+
+        startCoords = randEdge()
+
         pass
 
     def length(self):
@@ -16,3 +32,4 @@ class Star():
             Returns how long the "star" stays on screen for
         '''
         pass
+
